18435ad3ac6d3d1e6b47721fe2264ecdead1151f
deepan-crypto <deepang467@gmail.com>
2026-01-30 23:22:08 +0530

    updated ui

---
+++ frontend/src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Router from './router';
+import './App.css';
+
+createRoot(document.getElementById('root')).render(<Router />);
